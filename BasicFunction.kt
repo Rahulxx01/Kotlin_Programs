217485a593705abb7f2de73eea425366e15cd530
RAHUL YADAV <ryadav531996@gmail.com>
2018-03-31 22:03:54 +0530

    Basic funtion Updated
    Basic funtion Updated

--- BasicFunction.kt
+++ BasicFunction.kt
@@ -5,11 +5,15 @@ fun main(args:Array<String>){
     println("Enter b:")
     var b:Int = readLine()!!.toInt()
     var r = sum(a,b)
-    println("Sum of a and b is : $r")
+   display(r)
 }
 
-fun sum(a:Int,b:Int):Int?{
-    var sum:Int? = a+b
+fun display(sum:Int):Unit{
+    println("The sum is $sum")
+}
+
+fun sum(a:Int,b:Int):Int{
+    var sum:Int = a+b
     return sum
 
 }
